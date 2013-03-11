9e61368c08b7f7b132efc32d833e486958cc0258
Pucong Han <ph2369@columbia.edu>
2013-03-11 18:53:34 -0400

    Fixing the subtraction

--- lexer.py
+++ lexer.py
@@ -5,9 +5,6 @@ tokens = (
    'AND',
    'OR',
    'NOT',
-   'WITH',
-   'RIGHTSQUAREBRACKET',
-   'LEFTSQAREBRACKET',
    'PLUS',
    'MINUS',
    'URL',
@@ -24,28 +21,116 @@ tokens = (
    'FINDURL',
    'FINDTEXR',
    'COMBINE',
+   'DEF'
    'IF',
    'ELSE',
    'FOR',
+   'IS',
+   'WITH',
+   'INTO',
+   'IN',
+   'RIGHTSQUAREBRACKET',
+   'LEFTSQAREBRACKET',
+   'EMPTY',
 )
 
 t_PLUS    = r'\+'
 t_MINUS   = r'-'
 
 def t_AND(t):
-    r'(and) | (AND)'   
+    r'(?<=\s)((and) | (AND))(?=\s)'   
     return t
 
 def t_OR(t):
-    r'(or) | (OR)'   
+    r'(?<=\s)((or) | (OR))(?=\s)'   
     return t
 
 def t_NOT(t):
-    r'(not) | (NOT)'   
+    r'(?<=\s)((not) | (NOT))(?=\s)'   
+    return t
+
+def t_URL(t):
+    r'((url) | (URL))(?=\s)'   
+    return t
+
+def t_TEXT(t):
+    r'((text) | (TEXT))(?=\s)'   
+    return t
+
+def t_NUMBER(t):
+    r'((number) | (NUMBER))(?=\s)'   
+    return t
+
+def t_URLLIST(t):
+    r'((urllist) | (URLLIST) | (urlList))(?=\s)'   
+    return t
+
+def t_TEXTLIST(t):
+    r'((textlist) | (TEXTLIST) | (textList))(?=\s)'   
+    return t
+
+def t_NUMLIST(t):
+    r'((numlist) | (NUMLIST) | (numList))(?=\s)'   
+    return t
+
+def t_PRINT(t):
+    r'((print) | (PRINT))(?=\s)'   
+    return t
+
+def t_READ(t):
+    r'((read) | (READ))(?=\s)'   
+    return t
+
+def t_SAVE(t):
+    r'((save) | (SAVE))(?=\s)'   
+    return t
+
+def t_APPEND(t):
+    r'((append) | (APPEND))(?=\s)'   
+    return t
+
+def t_ADD(t):
+    r'((add) | (ADD))(?=\s)'   
+    return t
+
+def t_FINDURL(t):
+    r'((findurl) | (FINDURL) | (findURL))(?=\s)'   
+    return t
+
+def t_COMBINE(t):
+    r'((combine) | (COMBINE))(?=\s)'   
+    return t
+
+def t_DEF(t):
+    r'((def) | (DEF))(?=\s)'   
+    return t
+
+def t_IF(t):
+    r'((if) | (IF))(?=\s)'   
+    return t
+
+def t_ELSE(t):
+    r'((else) | (ELSE))(?=\s)'   
+    return t
+
+def t_FOR(t):
+    r'((for) | (FOR))(?=\s)'   
+    return t
+
+def t_IS(t):
+    r'(?<=\s)((is) | (IS))(?=\s)'
     return t
 
 def t_WITH(t):
-    r'(with) | (WITH)'
+    r'(?<=\s)((with) | (WITH))(?=\s)'
+    return t
+
+def t_INTO(t):
+    r'(?<=\s)((into) | (INTO))(?=\s)'
+    return t
+
+def t_IN(t):
+    r'(?<=\s)((in) | (IN))(?=\s)'
     return t
 
 def t_LEFTSQAREBRACKET(t):
@@ -56,68 +141,8 @@ def t_RIGHTSQUAREBRACKET(t):
     r'\]'
     return t
 
-def t_URL(t):
-    r'(url) | (URL)'   
-    return t
-
-def t_TEXT(t):
-    r'(text) | (TEXT)'   
-    return t
-
-def t_NUMBER(t):
-    r'(number) | (NUMBER)'   
-    return t
-
-def t_URLLIST(t):
-    r'(urllist) | (URLLIST) | (urlList)'   
-    return t
-
-def t_TEXTLIST(t):
-    r'(textlist) | (TEXTLIST) | (textList)'   
-    return t
-
-def t_NUMLIST(t):
-    r'(numlist) | (NUMLIST) | (numList)'   
-    return t
-
-def t_PRINT(t):
-    r'(print) | (PRINT)'   
-    return t
-
-def t_READ(t):
-    r'(read) | (READ)'   
-    return t
-
-def t_SAVE(t):
-    r'(save) | (SAVE)'   
-    return t
-
-def t_APPEND(t):
-    r'(append) | (APPEND)'   
-    return t
-
-def t_ADD(t):
-    r'(add) | (ADD)'   
-    return t
-
-def t_FINDURL(t):
-    r'(findurl) | (FINDURL) | (findURL)'   
-    return t
-
-def t_COMBINE(t):
-    r'(combine) | (COMBINE)'   
-    return t
-
-def t_IF(t):
-    r'(if) | (IF)'   
-    return t
-
-def t_ELSE(t):
-    r'(else) | (ELSE)'   
-    return t
-
-def t_FOR(t):
-    r'(for) | (FOR)'   
+def t_EMPTY(t):
+    r'(empty) | (EMPTY)'
     return t
 
 # A string containing ignored characters (spaces and tabs)
@@ -132,7 +157,7 @@ def t_error(t):
 lexer = lex.lex()
 
 # Give the lexer some input
-lexer.input("filterresult is findURL in stotries with term1 and term2")
+lexer.input("filterresult is findURL in stotries with term1 and term2 intothis into ")
 
 while True:
     tok = lexer.token()
