82db52eb35393c5f43c59868be68030a5f07cb19
Vicki <vm2355@columbia.edu>
2013-04-06 11:48:25 -0400

    printing list and vals

--- parser.py
+++ parser.py
@@ -23,31 +23,33 @@
 import yacc
 from lexer import tokens
 
-class Node:
-    def __init__(self,type,children=None,value=None):
-         self.type = type
-         if children:
-              self.children = children
-         else:
-              self.children = [ ]
-         self.value = value
+# class Node:
+#     def __init__(self,type,children=None,value=None):
+#          self.type = type
+#          if children:
+#               self.children = children
+#          else:
+#               self.children = [ ]
+#          self.value = value
 
-    def printrec(self):
-        for child in self.children:
-            child.printrec()
-        print self.type, self.value
+#     def printrec(self):
+#         for child in self.children:
+#             child.printrec()
+#         print self.type, self.value
 
-def p_expression_print(p):
+def p_expression_printlist(p):
     'expression : PRINT LIST'
-    print "Found a print statement"
-    p[0] = Node(p[1], [p[2]],'list')
-    return p[0]
+    print "Found a list print statement"
+    p[0] = ("print", p[2])
+
+def p_expression_printvals(p):
+    'expression : PRINT VALS'
+    print "Found a val print statement"
+    p[0] = ("print", p[2])
 
 def p_expression_list(p):
     'LIST : LEFTSQUAREBRACKET LISTITEMS RIGHTSQUAREBRACKET'
-    p[0] = Node("list", [], p[2:-1])
-    for item in p:
-        print item
+    p[0] = ("list", p[2:-1])
 
 def p_expression_listitems(p):
     '''LISTITEMS : IDENTIFIER EXTRALIST
@@ -58,6 +60,13 @@ def p_expression_extralist(p):
     '''EXTRALIST : COMMA IDENTIFIER EXTRALIST
                  | empty'''
     p[0] = p[1:]
+
+def p_expression_vals(p):
+    '''VALS : URL VALS
+            | TEXT VALS
+            | NUMBER VALS
+            | empty'''
+    p[0] = ("vals", p[])
 
 def p_empty(p):
     'empty :'
