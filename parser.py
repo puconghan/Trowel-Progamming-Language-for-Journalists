5bbe68c90bcab995e7dd62e6987e24727f7878f3
Pucong Han <ph2369@columbia.edu>
2013-04-06 17:08:34 -0400

    parser.py optimized and cleaned

--- parser.py
+++ parser.py
@@ -41,7 +41,7 @@ def p_additionalurl(p):
     # Adding variable name and type to the typelist.
     typelist.addNewType(p[2], "URLLIST")
 
-def p_additional(p):
+def p_additionalempty(p):
     'additionalurl : empty'
     pass
 
