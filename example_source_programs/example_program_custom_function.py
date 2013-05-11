53fab0ccfabf80a4739e99c43e4cf5fb9c26baa4
Robert Walport <robertwalport@gmail.com>
2013-05-11 14:40:24 -0400

    more parser tests in pre-commit particularly in regards to custom functions

--- example_source_programs/example_program_custom_function.py
+++ example_source_programs/example_program_custom_function.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python
 import os, sys
-sys.path.append("/Users/puconghan/Desktop/plt")
+sys.path.append("/Users/RWalport/Desktop/Rob_CS/PLT/plt")
 import trowelfunctions as tfl
-
 def gettweets(of,person,searching,twitter):
 	if not tfl.checktype(['text', 'text', 'text', 'text'],list(reversed(locals().values()))): return 'gettweets is used improperly'
-
 	prefix = ""
-	prefix = 'http://twitter.com/?api='
-
+	prefix = 'https://twitter.com/'
 	twitterurl = ""
 	tmp0 = prefix
 	tmp1 = 'and'
 	tmp2 = person
 	tfl.r_combine([tmp0,tmp1,tmp2])
 	twitterurl = tfl.r_combine([tmp0,tmp1,tmp2])
-
 	results = ""
 	tmp0 = 'in'
 	tmp1 = twitterurl
 	tfl.r_findtext([tmp0,tmp1])
 	results = tfl.r_findtext([tmp0,tmp1])
-
 	return results
-
 tweets = ""
 tmp0 = 'of'
 tmp1 = 'barackobama'
@@ -31,6 +25,5 @@ tmp2 = 'searching'
 tmp3 = 'twitter'
 gettweets(tmp0,tmp1,tmp2,tmp3)
 tweets = gettweets(tmp0,tmp1,tmp2,tmp3)
-
 tmp0 = tweets
 tfl.r_print([tmp0])
